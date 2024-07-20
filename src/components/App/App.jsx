dea362cf074ee5a44c2e25fe8526cd0e9656e67d
AndreyTara <explorituse@gmail.com>
2024-07-20 18:18:36 +0300

    'empty-css-App'

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+// import viteLogo from "/vite.svg";
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <div className={css.root}>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
+        debitis.
+      </p>
+    </div>
+  );
+}
+
+export default App;
